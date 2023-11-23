001fc91e7f0b38c95dd2bac675b86f006f39e302
emilos94 <emilrefsgaardmiddelboe@gmail.com>
2023-11-23 19:24:41 +0100

    add arraylist

---
+++ src/datastructures/arraylist.c
@@ -0,0 +1,147 @@
+#include "arraylist.h"
+
+// forward decl.
+void _arraylist_sort(ArrayList* list, int low, int high, int (comparison_func)(void*, void*));
+uint32_t _arraylist_sort_partition(ArrayList* list, int low, int high,  int (comparison_func)(void*, void*));
+void _arraylist_swap(ArrayList* list, uint32_t i, uint32_t j);
+
+
+ArrayList* arraylist_create(uint32_t capacity, uint32_t element_byte_size) {
+    ArrayList* list = malloc(sizeof(ArrayList));
+    list->data = malloc(capacity * element_byte_size);
+    list->capacity = capacity;
+    list->element_byte_size = element_byte_size;
+    list->count = 0;
+    return list;
+}
+
+void arraylist_add(ArrayList* list, void* entry) {
+    if (list->count == list->capacity) {
+        // realign
+        list->capacity *= 2;
+        list->data = realloc(list->data, list->capacity * list->element_byte_size);
+    }
+    
+    memcpy(list->data + list->count * list->element_byte_size, entry, list->element_byte_size);
+    list->count++;
+}
+
+void* arraylist_push(ArrayList* list) {
+    if (list->count == list->capacity) {
+        // realign
+        list->capacity *= 2;
+        list->data = realloc(list->data, list->capacity * list->element_byte_size);
+    }
+    
+    void* ptr = list->data + list->count * list->element_byte_size;
+    list->count++;
+    return ptr;
+}
+
+void* arraylist_get(ArrayList* list, uint32_t index) {
+    assert(index < list->count);
+    
+    return list->data + index * list->element_byte_size;
+}
+
+ArrayList* arraylist_copy(ArrayList* list) {
+    ArrayList* cpy = arraylist_create(list->capacity, list->element_byte_size);
+    cpy->count = list->count;
+    memcpy(cpy->data, list->data, list->count * list->element_byte_size);
+    return cpy;
+}
+
+// Does not keep order of items, but retains a dense array list
+void arraylist_remove(ArrayList* list, uint32_t index) {
+    assert(index < list->count);
+
+    // decrement count and return if remove last item
+    if (index == list->count - 1) {
+        list->count--;
+        return;
+    }
+
+    // move last item to removed index spot
+    char* ptr_to_remove = list->data + index * list->element_byte_size;
+    char* ptr_last_element = list->data + (list->count-1) * list->element_byte_size;
+
+    memcpy(ptr_to_remove, ptr_last_element, list->element_byte_size);
+    list->count--;
+}
+
+char* arraylist_popback(ArrayList* list) {
+    assert(list->count > 0);
+
+    char* ptr = list->data + (list->count-1) * list->element_byte_size;
+    list->count--;
+    return ptr;
+}
+
+char* arraylist_peekback(ArrayList* list) {
+    assert(list->count > 0);
+
+    return list->data + (list->count-1) * list->element_byte_size;
+}
+
+void arraylist_free(ArrayList* list) {
+    free(list->data);
+    free(list);
+}
+
+
+void arraylist_sort(ArrayList* list, int (comparison_func)(void*, void*)) {
+    if (list->count <= 1) {
+        return;
+    }
+
+    _arraylist_sort(list, 0, list->count-1, comparison_func);
+}
+
+
+void _arraylist_sort(ArrayList* list, int low, int high, int (comparison_func)(void*, void*)) {
+    if (low >= high || low < 0) {
+        return;
+    }
+
+    int pivot = _arraylist_sort_partition(list, low, high, comparison_func);
+    _arraylist_sort(list, low, pivot - 1, comparison_func);
+    _arraylist_sort(list, pivot + 1, high, comparison_func);
+}
+
+uint32_t _arraylist_sort_partition(ArrayList* list, int low, int high,  int (comparison_func)(void*, void*)) {
+    char* pivot = arraylist_get(list, high);
+
+    int tmp_pivot = low - 1;
+
+    for (int j = low; j < high; j++) {
+        int comp = (*comparison_func)(arraylist_get(list, j), pivot);
+        uint8_t j_lessthan_eq = comp == -1 || comp == 0;
+        if (j_lessthan_eq) {
+            tmp_pivot++;
+            _arraylist_swap(list, tmp_pivot, j);
+        }
+    }
+
+    tmp_pivot++;
+    _arraylist_swap(list, tmp_pivot, high);
+    return tmp_pivot;
+}
+
+void _arraylist_swap(ArrayList* list, uint32_t i, uint32_t j) {
+    assert(i < list->count && j < list->count);
+
+    if (i == j) {
+        return;
+    }
+
+    char* tmp = malloc(sizeof(char) * list->element_byte_size);
+
+    char* ptr_i = arraylist_get(list, i);
+    char* ptr_j = arraylist_get(list, j);
+
+    memcpy(tmp, ptr_i, list->element_byte_size);
+    memcpy(ptr_i, ptr_j, list->element_byte_size);
+    memcpy(ptr_j, tmp, list->element_byte_size);
+
+    free(tmp);
+}
---
+++ src/datastructures/arraylist.h
@@ -0,0 +1,35 @@
+#ifndef ARRAYLIST_H
+#define ARRAYLIST_H
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct ArrayList ArrayList;
+struct ArrayList {
+    uint32_t capacity;
+    uint32_t element_byte_size;
+    uint32_t count;
+    
+    void* data;
+};
+
+ArrayList* arraylist_create(uint32_t capacity, uint32_t element_byte_size);
+void arraylist_add(ArrayList* list, void* entry);
+void* arraylist_push(ArrayList* list);
+void* arraylist_get(ArrayList* list, uint32_t index);
+ArrayList* arraylist_copy(ArrayList* list);
+void arraylist_remove(ArrayList* list, uint32_t index);
+char* arraylist_popback(ArrayList* list);
+char* arraylist_peekback(ArrayList* list);
+void arraylist_free(ArrayList* list);
+void arraylist_sort(ArrayList* list, int (comparison_func)(void*, void*));
+
+#define ARRAYLIST_FOREACH(list, type, name)\
+    type* name = 0;\
+    int index = 0;\
+    for(index = 0, name = list->data + index * list->element_byte_size; index < list->count; index++, name = list->data + index * list->element_byte_size)
+
+#endif
